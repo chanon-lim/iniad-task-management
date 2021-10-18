cf84953378749c147c66d812149d4c8f07e32646
chanon-mike <s1f102100041@iniad.org>
2021-10-18 17:01:03 +0700

    Update index.html

--- myscript.js
+++ myscript.js
@@ -1,3 +1,4 @@
+// Sidebar to navbar (Responsive)
 let btn = document.querySelector("#collapse-btn");
 let navbar = document.querySelector(".mobile-nav");
 let content = document.querySelector(".content");
@@ -6,3 +7,8 @@ btn.onclick = function() {
     navbar.classList.toggle("active");
     content.classList.toggle("active");
 }
+
+// Date for today tab
+let today = new Date();
+let date = today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate();
+document.querySelector(".today-date").innerHTML = date;
