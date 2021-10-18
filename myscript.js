e964b561569799f950f0dd298c30a5df9bdbeec7
Tung <s1f102100029@iniad.org>
2021-10-18 15:48:57 +0700

    update

--- myscript.js
+++ myscript.js
@@ -6,3 +6,23 @@ btn.onclick = function() {
     navbar.classList.toggle("active");
     content.classList.toggle("active");
 }
+
+
+function openPage(pageName, elmnt, color) {
+    var tabcontent = document.getElementsByClassName("mytabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        // alert('This part fires'); //ok run
+        tabcontent[i].style.display = "none";
+    }
+
+    var links = document.getElementsByClassName("links");
+    for (i = 0; i < links.length; i++) {
+        links[i].style.backgroundColor = "";
+    }
+
+    // Show the specific tab content
+    document.getElementById(pageName).style.display = "block";
+
+    // Add the specific color to the button used to open the tab content
+    elmnt.style.backgroundColor = color;
+}
