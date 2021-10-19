ef02352497fc0b3877e8d74ce744cc4334f98067
YadanarLin <s1F102100030@iniad.org>
2021-10-19 20:18:29 +0630

    send request button, popup form, your request button
    More functions implemented. Some changes in design.

--- page.js
+++ page.js
@@ -1,16 +1,10 @@
-window.onscroll = function(){Sticky()};
 
-//Get the header
-var header=doument.getElementById('pageHeader');
 
-//Get the offset position of the navbar
-var sticky=header.offsetTop
-
-function Sticky(){
-    if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-      } else {
-        header.classList.remove("sticky");
-      }
+function openForm(){
+  document.getElementById('requestForm').style.display="block";
 }
-    
+
+function closeForm(){
+  document.getElementById('requestForm').style.display="none";
+
+}
